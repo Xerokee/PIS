13afdd669e9e9498d4a2fed18b68066f2df84f2e
Matija M <margeta.matija@gmail.com>
2024-05-06 17:30:35 +0200

    Fixes

--- PIS.Model/UsersDomain.cs
+++ PIS.Model/UsersDomain.cs
@@ -24,16 +24,11 @@ namespace PIS.Model
 
         public int UserId { get; set; }
         public string UserLoginName { get; set; }
-        [Required(ErrorMessage = "Unesite ime korisnika.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Ime korisnika mora biti između 3 i 50 slova")]
-        public string UserName { get; set; }
+                [Required(ErrorMessage = "Unesite ime korisnika.")]
+                [StringLength(50, MinimumLength = 3, ErrorMessage = "Ime korisnika mora biti između 3 i 50 slova")]
+                public string UserName { get; set; }
         public string UserSurname { get; set; }
         public string UserEmail { get; set; }
         public int? UserApproved { get; set; }
-
-        public static implicit operator UsersDomain(List<UsersDomain> v)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
