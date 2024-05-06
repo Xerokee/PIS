53c13b06415f3c95e1598780458be13a5d6b9284
Matija M <margeta.matija@gmail.com>
2024-05-06 16:41:25 +0200

    Primjer validacije na modelu

---
+++ PIS.Common/ErrorMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIS.Common
+{
+    public class ErrorMessage
+    {
+        public ErrorMessage(string message)
+        {
+        }
+        public ErrorMessage(ErrorMessage message)
+        {
+            Message = Message;
+        }
+
+        public string Message { get; set; }
+    }
+}
